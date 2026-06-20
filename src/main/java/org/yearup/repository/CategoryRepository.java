aa095cef4f39d99157c54c64337d4d1d3c1447a7
kadillon21 <kadillon21@gmail.com>
2026-06-20 08:22:55 -0500

    Methods in CategoryService have been completed as well as tweaking to CategoryRespoitory

--- src/main/java/org/yearup/repository/CategoryRepository.java
+++ src/main/java/org/yearup/repository/CategoryRepository.java
@@ -1,10 +1,15 @@
 package org.yearup.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.yearup.models.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer>
 {
+    @Query("SELECT name FROM categories")
+    List<Category> findAllCategories();
 }
